16d182a15824f8ae8ebffde872ba69ad998c1c6c
suhi6sun <suhitharan06@hotmail.co.uk>
2017-02-24 13:43:44 +0000

    os
    testing

---
+++ create_account.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<h1> My Hometown create_account </p>
+
+<form action="add-create_account.php" method="post">
+    Username: <input type = "text" name="username"><br>
+	E-mail: <input type = "text" name="email"><br>
+	passwords: <input type = "password" name="password" placeholder="password" required></li>
+	<input type="submit">
+
+
+</form>
+</body>
+</html>
